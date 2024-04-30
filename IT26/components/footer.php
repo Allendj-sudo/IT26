5a87d3cb2290d8f1bfaf2697cc5022d18b20d18f
Allendj-sudo <137299959+Allendj-sudo@users.noreply.github.com>
2024-04-30 15:05:49 +0800

    Add files via upload

---
+++ IT26/components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/mail.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:romeoallend@gmail.com">romeoallend@gmail.com</a>
+         <a href="mailto:qiimcagalawan@gmail.com">qiimcagalawan@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock.png" alt="">
+         <h3>opening hours</h3>
+         <p>7am to 10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Davao City,Philippines</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+         <a href="tel:1112223333">111-222-3333</a>
+      </div>
+
+   </section>
+
+   <div class="credit" <span>projectmangud</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/Aling.gif" alt="">
+</div>
